8fc80294913e14896ca7f837fd5a6ec7c58328b9
Philip Cheuk <Pcheuk@syr.edu>
2025-02-23 21:16:37 -0500

    Homework 3 Submission

--- code/one_package.py
+++ code/one_package.py
@@ -6,3 +6,20 @@ Calculate the total package size and display that.
 
 see one_package.png for a screenshot
 '''
+import streamlit as st
+import packaging
+
+st.title("Process One Package")
+
+package = st.text_input("Enter package data:")
+
+if package:
+    parsed_package = packaging.parse_packaging(package)
+    total = packaging.calc_total_units(parsed_package)
+    unit = packaging.get_unit(parsed_package)
+    st.text(parsed_package)
+    for item in parsed_package:
+        name = list(item.keys())[0]
+        size = list(item.values())[0]
+        st.info(f"{name} ➡️ {size}")
+    st.success(f"Total 📦 Size: {total} {unit}")
--- code/process_files.py
+++ code/process_files.py
@@ -8,3 +8,41 @@ summary information eg. "X packages written to file.json".
 
 Screenshot available as process_files.png
 '''
+import streamlit as st
+import packaging
+from io import StringIO
+import json
+
+if 'summaries' not in st.session_state:
+    st.session_state.summaries = []
+if 'total_lines' not in st.session_state:
+    st.session_state.total_lines = 0
+if 'total_files' not in st.session_state:
+    st.session_state.total_files = 0
+
+file = st.file_uploader("Upload Package File:")
+
+if file:
+    filename = file.name
+    json_filename = filename.replace(".txt", ".json")
+    packages = []
+    text = StringIO(file.getvalue().decode("utf-8")).read()
+    for line in text.split("\n"):
+        line = line.strip()
+        package = packaging.parse_packaging(line)
+        total = packaging.calc_total_units(package)
+        unit = packaging.get_unit(package)
+        packages.append(package)
+    count = len(package)
+    with open(f"./data/{json_filename}", "w") as f:
+        json.dump(packages, f, indent=4)
+    summary = f"{count} packages written to {json_filename}"
+
+    st.session_state.summaries.append(summary)
+    st.session_state.total_files += 1
+    st.session_state.total_lines += count
+
+    for s in st.session_state.summaries:
+        st.info(s, icon="💾")
+    st.success(f"{st.session_state.total_files} files processed, {st.session_state.total_lines} total lines processed")
+
